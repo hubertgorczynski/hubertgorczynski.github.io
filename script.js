db3dc6a54591eea486643b38e0b359788236ac8f
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-07-25 11:26:16 +0200

    Modified adress for Heroku and version of jquery 3.5.1 in html file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://polar-depths-19222.herokuapp.com//v1/task/';
+  const apiRoot = 'http://polar-depths-19222.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
