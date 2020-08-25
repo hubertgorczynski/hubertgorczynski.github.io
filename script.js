9e98c5d30c32e3a157cae065b49498d43a5ef2df
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-08-25 21:19:04 +0200

    Modified trelloApiRoot adress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://polar-depths-19222.herokuapp.com/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://polar-depths-19222.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
